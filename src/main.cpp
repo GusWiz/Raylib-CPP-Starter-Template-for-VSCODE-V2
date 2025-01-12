e406f190815ca6a8bc7c931c34ef29a716981167
Gustavo H <packy512@gmail.com>
2025-01-11 21:48:26 -0600

    Created Game Windows for Space Invaders Game

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,19 @@
-#include <raylib.h>
-#include "ball.h"
+#include "raylib.h"
 
+// Steps to creating Space Invaders.
+// 1. Create the Game Window. Which should be an empty window that we will use to draw
+// our game objects on.
+// 2. Create a Game Loop. 
+// 2.1 Event Handling.
+// 2.2 Update Positions such as players, aliens, and rocks.
+// 2.3 Draw the game objects.
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
-    {
-        ball.Update();
-        
-        BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
-        EndDrawing();
-    }
-    
+    // Specify the width and the height
+    int windowWidth = 750;
+    int windowHeight = 700;
+
+    InitWindow(windowWidth, windowHeight, "Space Invaders in C++");
+
     CloseWindow();
 }
